a79956ba25bfd4436f384b0ada3be94b3835acc8
murayama <murayama333@gmail.com>
2026-06-19 06:56:42 +0900

    exercise/03

--- src/main/java/com/example/demo/mapper/MessageMapper.java
+++ src/main/java/com/example/demo/mapper/MessageMapper.java
@@ -3,12 +3,11 @@ package com.example.demo.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+
 import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.entity.Message;
 
-@Mapper
 public interface MessageMapper {
 
 	@Select("SELECT * FROM message ORDER BY created_at DESC")
